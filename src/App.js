8da7e4ff3928421e22361742b386186687b068be
taejin.ahn <xowls0460@gmail.com>
2024-08-20 17:02:37 +0900

    tailwindcss 적용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { NavLink, Route, Routes } from "react-router-dom";
 import ListPage from "./pages/ListPage";
 import WritePage from "./pages/WritePage";
 
 function App() {
   return (
-    <div className="App">
-      <ListPage />
-      <WritePage />
-    </div>
+    <>
+      <header className="flex gap-2">
+        <NavLink to="/list" className="p-5 hover:text-red-500 font-bold">List</NavLink>
+        <NavLink to="/write" className="p-5 hover:text-red-500 font-bold">Write</NavLink>
+      </header>
+      <div className="App">
+        <Routes>
+          <Route path="/list" element={<ListPage />} />
+          <Route path="/write" element={<WritePage />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
--- src/Root.js
+++ src/Root.js
@@ -1,9 +1,12 @@
 import App from "./App";
+import { HashRouter } from "react-router-dom";
 
 function Root() {
   return (
     <>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </>
   );
 }
